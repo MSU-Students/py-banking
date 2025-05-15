f3d12055b19174baca3258ed935ebc2dcbed0434
Renalyn Joyce Bascones <renyuhin216@gmail.com>
2025-05-14 21:39:27 -0700

    al i check sa line 1501sa transaction_management.py oks?

--- transaction/transaction_management.py
+++ transaction/transaction_management.py
@@ -148,7 +148,7 @@ class TransactionService:
                     if account["account_number: "] == account_number:
                         print(f"User Id: {user_id}\nAccount_type: {account["account_type: "]},\nAccount Number: {account_number}\nCurrent balance: Php{account["account_balance: "]}")
                         print(f"christian ni gana ang pull?")
-                        
+                        print("ali nakita mo????")
                         break
         except Exception as e:
             print(f"An error occurred while checking balance: {e}")
