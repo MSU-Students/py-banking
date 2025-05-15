1be687cb96eb82d2ba7c05761f8457955cba519a
RenaLeen <renyuhin216@gmail.com>
2025-05-15 04:05:41 -0700

    christian yung sa exception handling, nag eeror ang withdrawal, na fix ko na kso may binago ako

--- transaction/transaction_management.py
+++ transaction/transaction_management.py
@@ -100,66 +100,68 @@ class TransactionService:
     def withdrawal(self, amount: float, user_id:str, account_type:str, account_number:str, account_balance:float):
         while True:
             if amount <= 0:
-                raise ValueError("Withdraw amount must be greater than zero.")
+                print("Withdraw amount must be greater than zero.")
+
             if amount > account_balance:
-                raise ValueError("Insufficient funds for withdrawal.")
+                print("Insufficient funds for withdrawal.")
+
             if account_balance - amount < 500:
-                raise ValueError("Wirhdrawal amount must not exceed the amount balance minus the maintaining balance of Php 500.")
-            if amount > 0 and account_balance - amount >= 500:
+                print("Wirhdrawal amount must not exceed the amount balance minus the maintaining balance of Php 500.")
+                
+            if account_balance - amount >= 500:       
+    
+                date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Include time
+                
+                account_balance -= amount  # Update the account's balance
+                transaction_number = str(random.randint(1000000, 9999999))
+                self.account = Transaction(user_id=user_id,account_type=account_type,account_number=account_number,type="withdrawal", date=date, amount=amount, transaction_number=transaction_number,original_balance=account_balance)
+                #para to sa original balance bago pa nag withdraw si user
+                self.account.original_balance += amount
+                
+                datas = {
+                    "account_number: ": self.account.account_number,
+                    "user_id: ": self.account.user_id,
+                    "account_type: ": self.account.account_type,
+                    "transaction_type: ": self.account.transaction_type,
+                    "date: ": self.account.date,
+                    "transaction_number: ": self.account.transaction_number,
+                    "original_balance: ": self.account.original_balance,
+                    "amount: ": self.account.amount
+                }
+                
+                #if meron siyang brackets, i load niya      
+                try:
+                    if os.path.exists(self.transaction_file):
+                        with open(self.transaction_file, 'r') as file:
+                            self.transactions_data = json.load(file)
+                #if walang laman talaga and transactions.json, as in walang brackets, mag lalagay siya ng empty bracket doon 
+                except(FileNotFoundError, json.JSONDecodeError):
+                        self.transactions_data = []
+                self.transactions_data.append(datas)
+                
+                #i store niya na append sa self.transactions_Data doon sa path na self.transactions.file
+                with open(self.transaction_file, 'w+') as transaction_file:
+                    json.dump(self.transactions_data, transaction_file, indent=4)
+            
+            
+                print(f"Deposited: {amount}. New balance: {account_balance}")
+                
+            #update the account balance of the selected user's account in accounts.json
+                with open("accounts.json", 'r') as file:
+                    accounts = json.load(file)
+                    for i, account in enumerate(accounts):
+                        if account["account_number: "] == account_number:
+                            accounts[i]["account_balance: "] = account_balance
+                            with open("accounts.json", 'w') as file:  
+                                pass
+                            break
+                with open("accounts.json", 'a') as file:  
+                    json.dump(accounts, file, indent=4)
+
+                print(f'\n\tSucessful Transaction!\nAccount Type: {self.account.account_type}\t Account Number: {self.account.account_number}\t Transaction Number: {self.account.transaction_number}')
                 break
-        
-            date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Include time
             
-            account_balance -= amount  # Update the account's balance
-            transaction_number = str(random.randint(1000000, 9999999))
-            self.account = Transaction(user_id=user_id,account_type=account_type,account_number=account_number,type="withdrawal", date=date, amount=amount, transaction_number=transaction_number,original_balance=account_balance)
-            #para to sa original balance bago pa nag withdraw si user
-            self.account.original_balance += amount
-            
-            datas = {
-                "account_number: ": self.account.account_number,
-                "user_id: ": self.account.user_id,
-                "account_type: ": self.account.account_type,
-                "transaction_type: ": self.account.transaction_type,
-                "date: ": self.account.date,
-                "transaction_number: ": self.account.transaction_number,
-                "original_balance: ": self.account.original_balance,
-                "amount: ": self.account.amount
-            }
-            
-            #if meron siyang brackets, i load niya      
-            try:
-                if os.path.exists(self.transaction_file):
-                    with open(self.transaction_file, 'r') as file:
-                        self.transactions_data = json.load(file)
-            #if walang laman talaga and transactions.json, as in walang brackets, mag lalagay siya ng empty bracket doon 
-            except(FileNotFoundError, json.JSONDecodeError):
-                    self.transactions_data = []
-            self.transactions_data.append(datas)
-            
-            #i store niya na append sa self.transactions_Data doon sa path na self.transactions.file
-            with open(self.transaction_file, 'w+') as transaction_file:
-                json.dump(self.transactions_data, transaction_file, indent=4)
-        
-        
-            print(f"Deposited: {amount}. New balance: {account_balance}")
-            
-           #update the account balance of the selected user's account in accounts.json
-            with open("accounts.json", 'r') as file:
-                accounts = json.load(file)
-                for i, account in enumerate(accounts):
-                    if account["account_number: "] == account_number:
-                        accounts[i]["account_balance: "] = account_balance
-                        with open("accounts.json", 'w') as file:  
-                            pass
-                        break
-            with open("accounts.json", 'a') as file:  
-                json.dump(accounts, file, indent=4)
-
-            print(f'\n\tSucessful Transaction!\nAccount Type: {self.account.account_type}\t Account Number: {self.account.account_number}\t Transaction Number: {self.account.transaction_number}')
-
-        else:
-            print("Withdrawal amount must be greater than PhP 500 Maintaining Balance and must not be less than 0.")
+            return
     # group 2 -christian (handling insuffiecient errors ) - ikaw na bahala mag gawa ng while loop dito
     # IKAW na bahala sano history transaction kapag ang account number walang laman na transaction history, gawan mo ng exception handling, and yung mga error messages
     def display_transactions(self, user_id:str, account_type:str, account_number:str):
@@ -168,21 +170,21 @@ class TransactionService:
         print(f"Account Number:{account_number}")
         print(f"\n\t\tList of Transactions\n")   
 
-    try:
-          with open("transactions.json", 'r') as file:
-             transactions = json.load(file)
-             i = 0
-             for transaction in transactions: 
-            
-                if transaction["user_id: "] == user_id and transaction["account_number: "] == account_number:
-                    print(f"{i+1.}\n* Date and Time: {transaction["date: "]} \n* Transaction Type: {transaction["transaction_type: "]} \n* Amount: {transaction["amount: "]}\n* Transaction Number: {transaction["transaction_number: "]}")
-                    i+=1
-             if i == 0:
-                raise ValueError("No transactions found for this account.")
-    except FileNotFoundError as e:
-        print(e)
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        try:
+            with open("transactions.json", 'r') as file:
+                transactions = json.load(file)
+                i = 0
+                for transaction in transactions:
+
+                    if transaction["user_id: "] == user_id and transaction["account_number: "] == account_number:
+                        print(f"{i+1.}\n* Date and Time: {transaction["date: "]} \n* Transaction Type: {transaction["transaction_type: "]} \n* Amount: {transaction["amount: "]}\n* Transaction Number: {transaction["transaction_number: "]}")
+                        i+=1
+                if i == 0:
+                    raise ValueError("No transactions found for this account.")
+        except FileNotFoundError as e:
+            print(e)
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
     
     def balance_inquiry(self, user_id:str,account_number:str):
         try:
@@ -193,9 +195,8 @@ class TransactionService:
                         print(f"User Id: {user_id}")
                         print(f"Account_type:  {account["account_type: "]}")
                         print(f"Account Number: {account_number}")
-                        print(f"Current balance: Php{account["account_balance: "]}")
-                        return
-                    
+                        print(f"Current balance: Php {account["account_balance: "]}")
+                        return        
         except Exception as e:
             print(f"An error occurred while checking balance: {e}")
         except FileNotFoundError as e:
